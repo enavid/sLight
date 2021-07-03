2ebff51905dc1db1ade0a5e351d419943070bfc9
enavid <en.navid@outlook.com>
2021-07-03 19:22:05 +0430

    handle light-threshold and lamp-brightness request from client

--- server/client/app.h
+++ server/client/app.h
@@ -11,12 +11,15 @@ const lampStatus = getItem('lamp-status');
 
 // Get Sensor from NodeMCU server
 setInterval(async function () {
-    const urls = ['/light', '/status']
-    const [light, status] = await Promise.all(urls.map(url =>
+    const urls = ['/light', '/status','/lightThreshold','/lightBrightness']
+    const [light, status, thresholdValue, brightnessValue] = await Promise.all(urls.map(url =>
         fetch(url).then(resp => resp.json())
     ));
-    Illuminance.innerHTML = "Illuminance " + light.response + " lux";
+    
+    Illuminance.innerHTML = "Illuminance = " + light.response + " lux";
     lampStatus.innerHTML = "Lamp-status = " + status.response;
+    brightness.innerHTML = "Lamp-brightness = " + brightnessValue.response + " %";
+    threshold.innerHTML = "Light-threshold = " + thresholdValue.response + " %";
 }, 2000);
 
 // Add Event listener to Button and send data to NodeMCU server
@@ -29,7 +32,7 @@ offButton.addEventListener('click', () => {
 
 // Add event listener to element
 slider1.addEventListener('input', () => {
-    brightness.innerHTML = "Light-brightness = " + slider1.value + " %";
+    brightness.innerHTML = "Lamp-brightness = " + slider1.value + " %";
 });
 
 slider2.addEventListener('input', () => {
@@ -38,11 +41,11 @@ slider2.addEventListener('input', () => {
 
 // Send event listener data to NodeMCU server
 slider1.addEventListener('change', () => {
-    fetch("?threshold=" + slider1.value, { mode: 'no-cors', method: 'POST' });
+    fetch("?brightness=" + slider1.value, { mode: 'no-cors', method: 'POST' });
 });
 
 slider2.addEventListener('change', () => {
-    fetch("?brightness=" + slider2.value, { mode: 'no-cors', method: 'POST' });
+    fetch("?threshold=" + slider2.value, { mode: 'no-cors', method: 'POST' });
 });
 
 )=====";
