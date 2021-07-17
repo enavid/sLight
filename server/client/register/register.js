7a486b5be427a0af02523fa6c52bfaba5c80e8fd
enavid <en.navid@outlook.com>
2021-07-17 13:00:29 +0430

    add register folder

---
+++ server/client/register/register.js
@@ -0,0 +1,19 @@
+const get = document.getElementById.bind(document);
+const button = get('submit');
+const user = get('username');
+const pass = get('psw');
+const confirmPass = get('confirm-psw');
+const ssid = get('ssid');
+const ssidPass = get('ssid-psw');
+
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    const username = user.value;
+    const password = pass.value;
+    const confirmPassword = confirmPass.value;
+    const ssidName = ssid.value;
+    const ssidPassword = ssidPass.value;
+    //if (username === '' || password === '' || confirmPassword === '' || ssidName === '' || ssidPassword === '') return alert('Please Check your input');
+    if (password !== confirmPassword) return alert('Your password dont match!');
+
+})
