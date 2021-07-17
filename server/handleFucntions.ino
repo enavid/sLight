064475b4259a6bdbb790755bb11276a8d2e9e0c2
enavid <en.navid@outlook.com>
2021-07-17 19:42:46 +0430

    handel eeprom

---
+++ server/handleFucntions.ino
@@ -0,0 +1,70 @@
+//******************************** Define web handle root function ************************
+
+void handleIndex(){server.send(200, "text/html", htmlPage);}
+void handleCSS(){ server.send(200, "text/css", stylePage);}
+void handleJS(){server.send(200, "text/js", appJS);}
+
+void handleLoginPage(){server.send(200, "text/html", loginIndex);}
+void handleLoginCSS(){server.send(200, "text/css", loginStyle);}
+void handleLoginJS(){server.send(200, "text/js", loginJS);}
+
+void handleRegisterPage(){server.send(200, "text/html", registerIndex);}
+void handleRegisterCSS(){server.send(200, "text/css", registerStyle);}
+void handleRegisterJS(){server.send(200, "text/js", registerJS);}
+
+void handleRegisterQuery(){
+  if(server.hasArg("username")) _username = server.arg("username");
+  if(server.hasArg("password")) _password = server.arg("password");
+  if(server.hasArg("ssid"))     _ssid     = server.arg("ssid");
+  if(server.hasArg("ssidpass")) _ssidPass = server.arg("ssidpass");
+
+  Serial.println(server.arg("username"));
+  Serial.println(server.arg("password"));
+  Serial.println(server.arg("ssid"));
+  Serial.println(server.arg("ssidpass"));
+
+  server.send(202);
+}
+  
+void handleMain(){handleIndex();}
+void handleResponse(){if(!handleRootQuery()){handleIndex();}}
+
+void handleRoot() {
+  Serial.println(server.header("Authorization"));
+  if(!server.hasHeader("Authorization")){return handleLoginPage();}
+  
+  //server.authenticate(_username, _password)? handleResponse() : server.send(403);
+}
+
+bool handleRootQuery(){
+  if(server.hasArg("threshold")) {
+    lightThreshold = server.arg("threshold").toInt();
+    return true;
+   }
+  if(server.hasArg("brightness")) {
+    lightBrightness = server.arg("brightness").toInt();
+    return true;
+   }
+  if(server.hasArg("lamp")){
+    if(server.arg("lamp").toInt() == 1) lightStatus = 1;
+    if(server.arg("lamp").toInt() == 0) lightStatus = 0;
+    return true; 
+  }
+  
+  return false;
+}
+
+void handleLight() {
+  server.send(200, "text/json", "{\"response\":" + String(light) + "}"); 
+}
+void handleLightThreshold() {
+  server.send(200, "text/json", "{\"response\":" + String(lightThreshold) + "}"); 
+}
+void handleLightBrightness() {
+  server.send(200, "text/json", "{\"response\":" + String(lightBrightness) + "}"); 
+}
+void handleStatus() {
+  lightStatus == 0 ?
+    server.send(200, "text/json", "{\"response\": \"OFF\"}"):
+    server.send(200, "text/json", "{\"response\": \"ON\"}");
+}
--- server/server.ino
+++ server/server.ino
@@ -1,5 +1,10 @@
+#include <EEPROM.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+
+
+#define LIGHTSENSORPIN A0
+#define PWMPIN D5
 
 //***************************************** Add external file to programm ************************
 #import "./client/register/registerIndex.h"
@@ -14,27 +19,23 @@
 #import "./client/index/app.h"
 #import "./client/index/style.h"
 
-#define LIGHTSENSORPIN A0
-#define PWMPIN D5
-
-
 //***************************************** Define global variable ************************
-unsigned long startMillis;  
-unsigned long currentMillis;
-const unsigned long period = 2000;  
-float light;
+
 int lightStatus = 0;
 int lightBrightness = 50;
 int lightThreshold = 50;
-int stateWork = 1; //0 for Register state 1 for normal state
-
-const char* www_username = "admin";
-const char* www_password = "admin";
+float light;
 
 String _username = "admin";
 String _password = "admin";
-String _ssid     = "navid";
+String _ssid     = "Navid";
 String _ssidPass = "wWw.shatel.@com";
+
+
+
+unsigned long startMillis;  
+unsigned long currentMillis;
+const unsigned long period = 2000;  
 
 IPAddress staticIP(192, 168, 1, 50);
 IPAddress gateway(192, 168, 1, 1);
@@ -42,104 +43,22 @@ IPAddress subnet(255, 255, 255, 0);
 
 ESP8266WebServer server(80);
 
-//================================== Function decelaration ===================================
 
-//******************************** Define web handle root function ************************
-
-void handleIndex(){server.send(200, "text/html", htmlPage);}
-void handleCSS(){ server.send(200, "text/css", stylePage);}
-void handleJS(){server.send(200, "text/js", appJS);}
-
-void handleLoginPage(){server.send(200, "text/html", loginIndex);}
-void handleLoginCSS(){server.send(200, "text/css", loginStyle);}
-void handleLoginJS(){server.send(200, "text/js", loginJS);}
-
-void handleRegisterPage(){server.send(200, "text/html", registerIndex);}
-void handleRegisterQuery(){
-  if(server.hasArg("username")) _username = server.arg("username");
-  if(server.hasArg("password")) _password = server.arg("password");
-  if(server.hasArg("ssid"))     _ssid     = server.arg("ssid");
-  if(server.hasArg("ssidpass")) _ssidPass = server.arg("ssidpass");
-
-  Serial.println(server.arg("username"));
-  Serial.println(server.arg("password"));
-  Serial.println(server.arg("ssid"));
-  Serial.println(server.arg("ssidpass"));
-
-  server.send(202);
-}
-  
-void handleRegisterCSS(){server.send(200, "text/css", registerStyle);}
-void handleRegisterJS(){server.send(200, "text/js", registerJS);}
-
-void handleMain(){
-  handleIndex();
-}
-
-void handleResponse(){
-  if(!handleRootQuery()){
-    handleIndex();
-  }
-}
-
-void handleRoot() {
-  Serial.println(server.header("Authorization"));
-  if(!server.hasHeader("Authorization")){return handleLoginPage();}
-  
-  server.authenticate(www_username, www_password)? handleResponse() : server.send(403);
-  
-}
-//************************************* Handle Function ***********************************
-
-bool handleRootQuery(){
-  if(server.hasArg("threshold")) {
-    lightThreshold = server.arg("threshold").toInt();
-    return true;
-   }
-  if(server.hasArg("brightness")) {
-    lightBrightness = server.arg("brightness").toInt();
-    return true;
-   }
-  if(server.hasArg("lamp")){
-    if(server.arg("lamp").toInt() == 1) lightStatus = 1;
-    if(server.arg("lamp").toInt() == 0) lightStatus = 0;
-    return true; 
-  }
-  
-  return false;
-}
-
-void handleLight() {
-  server.send(200, "text/json", "{\"response\":" + String(light) + "}"); 
-}
-void handleLightThreshold() {
-  server.send(200, "text/json", "{\"response\":" + String(lightThreshold) + "}"); 
-}
-void handleLightBrightness() {
-  server.send(200, "text/json", "{\"response\":" + String(lightBrightness) + "}"); 
-}
-void handleStatus() {
-  lightStatus == 0 ?
-    server.send(200, "text/json", "{\"response\": \"OFF\"}"):
-    server.send(200, "text/json", "{\"response\": \"ON\"}");
-}
-//
-//bool authorizationUser(String user, String pass){
-//  return userName.equals(user) && password.equals(pass);
-//}
 
 //  ================================= setup function ====================================
 void setup() {
+  EEPROM.begin(512);
+  Serial.begin(115200);
   pinMode(LIGHTSENSORPIN, INPUT);
 
-  Serial.begin(115200);
+  
   Serial.println();
 
   analogWriteFreq(55);
 
   //WiFi.config(staticIP, gateway, subnet);
 
-  WiFi.begin("Navid", "wWw.shatel.@com");
+  WiFi.begin(_ssid, _ssidPass);
   
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -151,7 +70,27 @@ void setup() {
   Serial.println();
   Serial.print("Connected , Ip address: ");
   Serial.println(WiFi.localIP());
+// ======================================= wirte EEPROM  ===================================  
+//  writeString(0, _username);
+//  delay(100);
+//  writeString(50, _password);
+//  delay(100);
+//  writeString(100, _ssid);
+//  delay(100);
+//  writeString(150, _ssidPass);
+//  delay(100);
   
+  String username = read_String(0);
+  String password = read_String(50);
+  String ssid     = read_String(100);
+  String ssidPass = read_String(150);
+
+  Serial.println(username);
+  Serial.println(password);
+  Serial.println(ssid);
+  Serial.println(ssidPass);
+  
+
 //======================================== Handle web root call ===========================
   //Handle root request
   server.on("/", handleRoot);
